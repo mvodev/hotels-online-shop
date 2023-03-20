782bccd2683019ef43d30af557c0b14efb2fe737
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-20 18:57:52 +0300

    add footer layout

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,8 +5,6 @@ const MainPage = () => {
   return (
     <section className="main-page">
       <h1 className='main-page__header'>Тестовое задание HOTELS.RU интернет магазин</h1>
-      <Logo mode='dark'/>
-      <Logo mode='white'/>
     </section>
   )
 }
