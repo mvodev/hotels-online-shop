2cb35f8b044a7a1eb2bae34cc751c49caddc4c0d
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-21 12:07:50 +0300

    add button component

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,3 +1,4 @@
+import Button from './components/button/Button';
 import './MainPage.scss';
 
 const MainPage = () => {
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,56 @@
+import './Button.scss';
+import deleteUrl from '../../assets/images/delete.svg';
+import cartUrl from '../../assets/images/cart.svg';
+import catalogUrl from '../../assets/images/catalog.svg';
+import downloadUrl from '../../assets/images/download.svg';
+
+export type ButtonProps = {
+  buttonType: 'common' | 'download' | 'download-wide' | 'cart' | 'delete'
+  text?:string,
+  href?:string,
+}
+
+const Button = (props:ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const { buttonType='common', text, href='mock-ddress-change-me.pdf' } = props;
+  let imgSrc = '';
+  let alt = '';
+  let buttonBody = <></>;
+  if (buttonType === 'delete') {
+    imgSrc = deleteUrl;
+    alt = 'trash image';
+  } else if (buttonType === 'cart') {
+    alt = 'cart image';
+    imgSrc = cartUrl;
+  } else if (buttonType === 'download') {
+    alt = 'catalog image';
+    imgSrc = catalogUrl;
+  } else if (buttonType === 'download-wide') {
+    alt = 'download image';
+    imgSrc = downloadUrl;
+  }
+  if (buttonType === 'common'){
+    buttonBody = <>
+      <span>{text}</span>
+    </>
+  } else if (buttonType === 'download' || buttonType === 'download-wide') {
+    buttonBody = <>
+      <a href={href} download>
+        <span>{text}</span>
+        <img src={imgSrc} alt={alt}/>
+      </a>
+    </>
+  } 
+  else {
+    buttonBody = <>
+      <span>{text}</span>
+      <img src={imgSrc} alt={alt}/>
+    </>
+  }
+  return (
+    <button className={`button button_${type}`}>
+      {buttonBody}
+    </button>
+  )
+}
+
+export default Button;
