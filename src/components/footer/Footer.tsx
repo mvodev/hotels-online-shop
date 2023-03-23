fdbd6eb71ad2ade12f5b3b48d7dff7949a943391
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-23 15:08:37 +0300

    add margins for footer elems

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,8 @@ import Button from '../button/Button';
 import Form from '../form/Form';
 import Logo from '../logo/Logo';
 import NavList from '../nav-list/NavList';
+import whatsUpLink from '../../assets/images/whatsup.svg';
+import telegramLink from '../../assets/images/telegram.svg';
 import './Footer.scss';
 export type FooterPropsType = {
   callbackRef:string
@@ -24,7 +26,7 @@ const Footer = (props:FooterPropsType) => {
       <div className="footer__right-side">
         <div className="footer__nav">
           <div className="footer__nav-wrapper">
-            <h2 className='footer__header'>Меню сайта:</h2>
+            <h3 className='footer__header'>Меню сайта:</h3>
               <NavList 
               mode='white' isVertical={true} 
               links={[
@@ -36,14 +38,14 @@ const Footer = (props:FooterPropsType) => {
             />
           </div>
           <div className="footer__nav-wrapper">
-            <h2 className='footer__header'>Категории:</h2>
+            <h3 className='footer__header'>Категории:</h3>
               <NavList 
               mode='white' isVertical={true} 
               links={[
                 {text:'О компании',href:'mock-address-change-me'},
                 {text:'Доставка и оплата',href:'mock-address-change-me'},
                 {text:'Возврат',href:'mock-address-change-me'},
-                {text:'Контакты',href:'mock-address-change-me'}
+                {text:'Контакты',href:'mock-address-change-me'},
               ]}
             />
           </div>
@@ -51,15 +53,35 @@ const Footer = (props:FooterPropsType) => {
         <div className="footer__price">
           <h3>Скачать прайс-лист:</h3>
           <Button buttonType='download-extra-wide' text='Прайс-лист'/>
+          <span>Связь в мессенджерах:</span>
+          <div className="footer__messengers">
+            <a 
+              className='footer__whatsup' 
+              href="http://mock-address-change-me" target={'_blank'}
+              rel={'noopener noreferrer'}
+            >
+              <img src={whatsUpLink} alt="whatsup image" />
+            </a>
+            <a 
+              className='footer__telegram' 
+              href="http://mock-address-change-me" 
+              target={'_blank'}
+              rel={'noopener noreferrer'}
+            >
+              <img src={telegramLink} alt="telegram image"/>
+            </a>
+          </div>
         </div>
         <div className="footer__contacts">
           <h3>Контакты:</h3>
           <address className="footer__address">
             <a className="footer__tel" href="tel:+77774900091">+7 (777) 490-00-91</a>
           </address>
-          <span>время работы: 9:00-20:00</span>
+          <div className="footer__work-hours">
+            время работы: <time dateTime='09:00'>9:00</time>-<time dateTime='20:00'>20:00</time>
+          </div>
           <a href={callbackRef}>Заказать звонок</a>
-          <address className="footer__address">
+          <address className="footer__address footer__address_email">
             <a className="footer__email" href="mailto:info@conquest.watch.ru">opt.sultan@mail.ru</a>
           </address>
           <span>На связи в любое время</span>
