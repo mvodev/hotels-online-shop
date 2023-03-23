a08be67dc16e2b441d8b0fa7b987a6e69b514a0c
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-23 22:38:59 +0300

    changed structure of main page

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,13 +1,26 @@
+import Card from './components/card/Card';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import image1Url from './assets/images/image1.png';
 import './MainPage.scss';
 
 const MainPage = () => {
   return (
-    <section className="main-page">
-      <Header callbackRef='mock-address-change-me'/>
-      <Footer callbackRef='mock-address-change-me'/>
-    </section>
+    <div className="wrapper">
+      <Header callbackRef='mock-address-change-me' />
+      <Card 
+        imgUrl={image1Url} 
+        quantity={'450 мл'} 
+        quantityImg={''} 
+        title={'AOS'} 
+        description='средство для мытья посуды Crystal'
+        barcode='4604049097548'
+        manufacturer='Нэфис'
+        brand='AOS'
+        price='48,76'
+      />
+      <Footer callbackRef='mock-address-change-me' />
+    </div>
   )
 }
 
