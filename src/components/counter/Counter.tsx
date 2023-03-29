c84fe861d1c189b64886accbf930864f8f662b18
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-29 09:48:53 +0300

    button add to card removed into counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,24 +1,35 @@
 import { useState } from 'react';
+import Button from '../button/Button';
 import './Counter.scss';
 
-const Counter = () => {
+export type CounterPropsType = {
+  handleClick: (counter:number)=>void,
+}
+
+const Counter = (props:CounterPropsType) => {
+  const { handleClick } = props;
   const [counter,setCounter] = useState(0);
+
   const handleCounter = (event:React.PointerEvent<HTMLButtonElement>) => {
     const isButtonMinusClicked = event.currentTarget.classList.contains('counter__button_minus');
+    let newCounterState = counter;
     if (isButtonMinusClicked) {
-      if (counter > 0) setCounter(counter-1); 
-    } else setCounter(counter+1);
+      if (counter > 0) newCounterState = counter-1; 
+    } else newCounterState =  counter+1;
+    setCounter(newCounterState);
   }
-
 
   return (
     <div className="counter">
       <button type='button' className='counter__button counter__button_minus' onPointerDown={handleCounter}>-</button>
       <span>{counter}</span>
       <button type='button' className='counter__button counter__button_plus' onPointerDown={handleCounter}>+</button>
+      <Button buttonType='cart-wide' text=' В КОРЗИНУ' onPointerDown={()=>{
+          setCounter(0);
+          handleClick(counter);
+        }}/>
     </div>
   )
-
 }
 
 export default Counter;
