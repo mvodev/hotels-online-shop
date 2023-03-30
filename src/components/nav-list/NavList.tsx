f3614ace3df1db85f198393d76e0e11c7f24364e
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-30 13:09:05 +0300

    add support burger menu into nav list

--- src/components/nav-list/NavList.tsx
+++ src/components/nav-list/NavList.tsx
@@ -1,7 +1,10 @@
 import './NavList.scss';
+import burgerUrl from '../../assets/images/burger.svg';
+import { useState } from 'react';
 
 export type NavListProps = {
   isVertical: boolean,
+  withBurger?:boolean,
   links:Array<{
     href:string,
     text:string,
@@ -10,7 +13,8 @@ export type NavListProps = {
 }
 
 const NavList = (props:NavListProps) => {
-  const {isVertical,links,mode} = props;
+  const { isVertical, links, mode , withBurger = false} = props;
+  const [navListClass,setNavListClass] = useState('');
   const items = links.map((link,index)=>{
     return( 
       <li className='nav-list__item' key={index}>
@@ -19,9 +23,18 @@ const NavList = (props:NavListProps) => {
     )
   })
 
+  const handleBurger = () => {
+    console.log('inside');
+    if (navListClass.length === 0) {
+      setNavListClass('nav-list__items_visible')
+    } else setNavListClass('');
+  }
+
   return (
-    <nav className={`nav-list ${isVertical? 'nav-list_vertival' : ''} ${mode==='dark'? 'nav-list_dark' : 'nav-list_white'}`}>
-      <ul className={`nav-list__items ${isVertical? 'nav-list__items_vertival' : ''}`}>
+    <nav className={`nav-list ${withBurger ? 'nav-list_burger' : ''} ${isVertical? 'nav-list_vertival' : ''} ${mode==='dark'? 'nav-list_dark' : 'nav-list_white'}`}>
+      {withBurger && <div className="nav-list__burger" onPointerDown={handleBurger}>
+        <img src={burgerUrl} alt="menu image" /></div>}
+      <ul className={`nav-list__items ${isVertical? 'nav-list__items_vertival' : ''} ${navListClass}`}>
         {items}
       </ul>
     </nav>
