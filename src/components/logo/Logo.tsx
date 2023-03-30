949473d8f319bb440263973234126d6453cf5ce2
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-30 17:36:28 +0300

    add small type for logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,13 +1,14 @@
 import './Logo.scss';
 
 export type LogoPropsType = {
-  mode: 'white' | 'dark'
+  mode: 'white' | 'dark',
+  small?: boolean
 }
 
 const Logo = (props:LogoPropsType) => {
-  const { mode } = props; 
+  const { mode, small = false } = props; 
   return (
-    <div className={`logo ${mode==='dark' ? 'logo_color_dark' : 'logo_color_white'}`}>
+    <div className={`logo ${mode==='dark' ? 'logo_color_dark' : 'logo_color_white'} ${small ? 'logo_small' : 'logo_common'}`}>
     </div>
   )
 }
