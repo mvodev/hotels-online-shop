a33016d7c23d4d61d91e4b08096c2f4ff646026e
Vadim Manushin <vadimmanushin@yandex.ru>
2023-04-02 18:21:39 +0300

    add bread crumbs for confirm order page

--- src/pages/ConfirmPage.tsx
+++ src/pages/ConfirmPage.tsx
@@ -1,8 +1,13 @@
+import BreadCrumbs from '../components/bread-crumbs/BreadCrumbs';
 import './ConfirmPage.scss';
 
 const ConfirmPage = () => {
   return (
     <main className="confirm">
+      <BreadCrumbs links={[
+        {link:'/',name:'Главная'},
+        {link: '',name:`${'Оформление заказа'}`,actual:true},
+      ]}/>
       <h1 className="confirm__header">Заказ оформлен</h1>
     </main>
   )
