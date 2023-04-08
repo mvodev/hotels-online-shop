672d3dab76978e5c6b1e41aaf0dd584785863e4d
Vadim Manushin <vadimmanushin@yandex.ru>
2023-04-08 20:52:13 +0300

    add test to check transition to page Admin when click on bread crumbs on Change page

--- src/pages/ChangePage.spec.tsx
+++ src/pages/ChangePage.spec.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ChangePage from './ChangePage';
-import { BrowserRouter } from "react-router-dom";
+
+import { MemoryRouter } from "react-router-dom";
 import '@testing-library/jest-dom';
+import AdminPage from './AdminPage';
 
 describe('Change page test correct heading',() => {
 
@@ -16,9 +19,9 @@ describe('Change page test correct heading',() => {
 
   test('renders Change Page correct heading without typeOfCard props', () => {
     render(
-    <BrowserRouter>
+    <MemoryRouter>
       <ChangePage />
-    </BrowserRouter>
+    </MemoryRouter>
     );
     expect(
       screen.getByRole('heading', { name: "Измените данные" })
@@ -27,13 +30,48 @@ describe('Change page test correct heading',() => {
 
   test('renders Change Page correct heading with typeOfCard props', () => {
     render(
-    <BrowserRouter>
+    <MemoryRouter>
       <ChangePage typeOfCard='new'/>
-    </BrowserRouter>
+    </MemoryRouter>
     );
     expect(
       screen.getByRole('heading', { name: "Введите данные" })
     ).toBeInTheDocument();
   });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  })
+
+})
+
+
+describe('Change page test correct transition after click on bread crumbs',() => {
+
+  beforeAll(()=>{
+    const mockedUsedNavigate = jest.fn();
+
+    jest.mock("react-router-dom", () => ({
+      ...(jest.requireActual("react-router-dom") as any),
+      useNavigate: () => mockedUsedNavigate
+    }));
+  })
+
+  test('router Change Page bread crunbs', () => {
+    render(
+    <MemoryRouter>
+      <AdminPage />
+      <ChangePage typeOfCard='new'/>
+    </MemoryRouter>
+    );
+
+    const link = screen.getByRole('link',{ name: "Страница администратора" });
+    userEvent.click(link);
+    expect(screen.getByRole('heading',{ name: "Страница администратора" }))
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  })
 
 })
